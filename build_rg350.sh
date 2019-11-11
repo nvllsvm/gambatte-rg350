5ececbffd0cb3761b4d403ca7c3d4e56fb3e7d1b
Andrew Rabert <ar@nullsum.net>
2019-11-11 11:43:23 -0500

    Add rg350 build script

---
+++ build_rg350.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+set -e
+
+BDAT="$(date +"%Y%m%d-%H%M%S")"
+
+cat > ./gambatte_sdl/builddate.h << EOF
+#define BUILDDATE "${BDAT}"
+EOF
+
+scons -Q target=gcw0 -C libgambatte
+scons -Q target=gcw0 -C gambatte_sdl
+
+mkdir -p build
+target="build/gambatte-gcw0-r572u3-${BDAT}.opk"
+
+mksquashfs \
+    ./dist/gcw0/default.gcw0.desktop \
+    ./gambatte_sdl/gambatte_sdl \
+    ./dist/gcw0/gambatte.png \
+    ./dist/gcw0/manual.txt \
+    "$target" -all-root -no-xattrs -noappend -no-exports
